e6b1e344cbf7b1056316ddc5bdcc78886bc8c0fd
Vishal Gowda <cartmanboy1991@gmail.com>
2020-02-02 01:23:17 +0100

    cleanup cycle det.

--- CycleDetection.java
+++ CycleDetection.java
@@ -1,4 +1,5 @@
-import java.io.IOException;
+
+
 class CycleDetection {
     static class Node {
 	int data;
@@ -17,35 +18,32 @@ class CycleDetection {
 	}
     }
 
-    static Node collissionPoint(Node head) throws IOException {
+    static Node collissionPoint(Node head) {
 	Node slow = head;
 	Node fast = head;
 	while(fast != null) {
-	    System.out.println("Slow: " + slow + ", Fast: " + fast);
 	    slow = slow.next;
 	    fast = fast.next.next;
-	    System.in.read();
 	    if (slow == fast) break;
 	}
-	System.out.println("slow and fast are: " + slow + ", " + fast );
+
 	return fast;
     }
 
-    static Node loopHead(Node head) throws IOException {
+    static Node loopHead(Node head) {
 	Node ptr1 = head;
 	Node ptr2 = collissionPoint(head);
-	System.out.println("coll point " + ptr2);
 	if (ptr2 == null) return null;
 	// move 2 pointers one from head, other from coll. point at same pace
 	while(ptr1 != ptr2) {
 	    ptr1 = ptr1.next;
 	    ptr2 = ptr2.next;
 	}
+
 	return ptr1;
-	
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 	Node loopHead = new Node(0);
 	loopHead.next = new Node(1, new Node(3, new Node(5, loopHead)));
 	Node list = new Node(4, new Node(2));
